3b4ecc7e8fa0d8f222c3da0c201be5c486402144
itsazzam <hire.azzam@gmail.com>
2024-06-14 01:40:51 +0300

    first

--- data-client/middleware.js
+++ data-client/middleware.js
@@ -1,46 +1,48 @@
-import { NextResponse } from 'next/server';
-import { getAccessToken } from '@/utils/webflow_helper';
+import { NextResponse } from "next/server";
+import { getAccessToken } from "@/utils/webflow_helper";
 
 /**
  * A middleware function that retrieves an access token from Webflow and sets it as a cookie
  * or redirects to the login page if the access token is missing.
- * 
+ *
  * @param {import('next/dist/next-server/server/api-utils').NextApiRequest} request - The Next.js API request object.
  * @returns {import('next/dist/next-server/server/api-utils').NextApiResponse} - The Next.js API response object.
  */
 
-const CLIENT_URL = process.env.CLIENT_URL
+const CLIENT_URL = process.env.CLIENT_URL;
 
-export async function middleware(request){ 
-  if (request.method === 'OPTIONS') {
+export async function middleware(request) {
+  if (request.method === "OPTIONS") {
     // Preflight request. Reply successfully:
 
     return new NextResponse(null, {
       headers: {
-        'Access-Control-Allow-Origin': "*",
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
       },
     });
+  } else {
+    console.error("TEST ERROR");
   }
-    else {
-      console.error("TEST ERROR")
-    }
-  
-  if (request.nextUrl.pathname === '/webflow_redirect' && request.nextUrl.searchParams.get('code')) {
+
+  if (
+    request.nextUrl.pathname === "/webflow_redirect" &&
+    request.nextUrl.searchParams.get("code")
+  ) {
     try {
-      const code = request.nextUrl.searchParams.get('code');
+      const code = request.nextUrl.searchParams.get("code");
       const token = await getAccessToken(code);
       if (token) {
         // If the access token is retrieved successfully, set it as a cookie and return the response
         const response = NextResponse.next();
-        response.cookies.set('webflow_auth', token, {
+        response.cookies.set("webflow_auth", token, {
           // httpOnly: true,
           // secure: true,
           // sameSite: 'strict',
-          maxAge: 60 * 60 * 24 * 30 // 30 days
+          maxAge: 60 * 60 * 24 * 30, // 30 days
         });
-        response.cookies.set('authenticated', 'true');
+        response.cookies.set("authenticated", "true");
         return response;
       }
     } catch (error) {
@@ -51,12 +53,15 @@ export async function middleware(request){
     }
   }
 
-  if (request.nextUrl.pathname.startsWith('/api')) {
+  if (request.nextUrl.pathname.startsWith("/api")) {
     return NextResponse.next();
   }
 
-  if (request.nextUrl.pathname !== '/login' && !request.cookies.get('webflow_auth')) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (
+    request.nextUrl.pathname !== "/login" &&
+    !request.cookies.get("webflow_auth")
+  ) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
@@ -64,7 +69,5 @@ export async function middleware(request){
 
 // Run this middleware on all pages except /login
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
-  ]
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
